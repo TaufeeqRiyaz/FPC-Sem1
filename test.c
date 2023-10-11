574e4a347c6a1bc3972079569c4a43c0c5d57b53
Taufeeq <taufeeq.riyaz64@gmail.com>
2023-10-11 10:50:15 +0530

    i'm bored i n class ahhhhhh

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i =10;
+    for (i = 10; i>=1; i /=2)
+    printf("%d", i++);
+}
---
+++ test1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i;
+    float j;
+
+    i = 5;
+    j = (float)(5/2);
+    printf("%f \n", j);
+    
+
+}
