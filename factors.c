7b3de9a5f5f8b3d7197ff438395eebe601f366cc
Taufeeq <taufeeq.riyaz64@gmail.com>
2023-10-18 07:20:15 +0530

    anjkfhdjkhfd

--- factors.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-int i = 5, j =5;
-printf(i+++++j); 
-}
---
+++ preincrement.c
@@ -0,0 +1,11 @@
+// preincrement in c
+
+#include <stdio.h>
+
+int main()
+
+{
+    int a = 6;
+    printf("The value of a is %d\n", ++a);
+    printf("The value of a is %d\n", a);
+}
