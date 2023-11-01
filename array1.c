48c2627bf493bda36444e00596e4da186f7afc81
Taufeeq <taufeeq.riyaz64@gmail.com>
2023-11-01 21:39:35 +0530

    idk what this is

---
+++ array1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+int n;
+printf("Enter the size of the array - ");
+scanf("%d",&n);
+
+int arr[n];
+printf("Enter the elements of the array - ");
+
+for(int i=0;i<n;i++)
+{
+    scanf("%d",&arr[i]);
+}
+
+printf("The elements of the array are - ");
+
+for(int i=0;i<n;i++)
+{
+printf("%d ",arr[i]);
+
+}
+}
