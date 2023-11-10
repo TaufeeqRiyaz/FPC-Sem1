34a726a7fd8195114cff1515b742b3cbb6405743
Taufeeq <taufeeq.riyaz64@gmail.com>
2023-11-11 00:53:17 +0530

    adding files

---
+++ CO4/Act-1/1.c
@@ -0,0 +1,16 @@
+// Write a program to sum of all prime numbers from 1 to n. Take the value of n from the user at runtime.
+
+
+
+#include <stdio.h>
+
+int main(){
+
+int n;
+int sum = 0;
+
+printf("Enter the value of n - ");
+scanf("%d", &n);
+
+
+}
